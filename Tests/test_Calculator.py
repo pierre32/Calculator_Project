6cbd89bdf6c61d72b8822a3538cde513e0259ff6
pierre32 <71328612+pierre32@users.noreply.github.com>
2020-11-07 22:58:10 -0500

    Update test_Calculator.py

--- Tests/test_Calculator.py
+++ Tests/test_Calculator.py
@@ -35,21 +35,7 @@ class MyTestCase(unittest.TestCase):
             self.assertEqual(self.calculator.divide(row['Value 2'], row['Value 1']), float(row['Result']))
             self.assertEqual(self.calculator.result, float(row['Result']))
 
-    def test_square(self):
-        test_data = CsvReader('/src/src/data/square.csv').data
-        for row in test_data:
-            self.assertEqual(self.calculator.square(row['Value 1']), int(row['Result']))
-            self.assertEqual(self.calculator.result, int(row['Result']))
 
-    def test_square_root(self):
-        test_data = CsvReader('/src/src/data/square_root.csv').data
-        for row in test_data:
-            self.assertTrue(
-                abs(self.calculator.square_root(row['Value 1']) - float(row['Result'])) < 0.0000001
-            )
-            self.assertTrue(
-                abs(self.calculator.result - float(row['Result'])) < 0.0000001
-            )
 
 
 if __name__ == '__main__':
