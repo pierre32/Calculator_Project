6c26a4fd75db59930afb1fb33b902d904efcfd3f
pierre32 <71328612+pierre32@users.noreply.github.com>
2020-11-07 22:47:07 -0500

    Update Calculator.py

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -15,10 +15,4 @@ class Calculator:
         self.result = round(int(a) / int(b), 9)
         return self.result
 
-    def square(self, a):
-        self.result = int(a) * int(a)
-        return self.result
-
-    def square_root(self, a):
-        self.result = round(int(a) ** 0.5, 7)
-        return self.result
+    
